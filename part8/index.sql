c05135dcc7fd861f77cb88da891bf4829fb4588a
Tarek <tarek.elouatar@gmail.com>
2018-01-19 11:50:07 +0100

    terminer

--- part8/index.sql
+++ part8/index.sql
@@ -2,18 +2,33 @@
 
 --Afficher tous les frameworks associés à leurs langages. Si un langage n'a pas de framework l'afficher aussi.
 SELECT languages.name, frameworks.name
--> FROM languages
--> LEFT JOIN frameworks
+-> FROM `languages`
+-> LEFT JOIN `frameworks`
 -> ON languages.id = frameworks.languagesId;
 
 -- Exercice 2 --
 
 --Afficher tous les frameworks associés à leurs langages. Si un langage n'a pas de framework ne pas l'afficher.
 SELECT languages.name, frameworks.name
--> FROM languages
+-> FROM `languages`
 -> INNER JOIN frameworks
 -> ON languages.id = frameworks.languagesId;
 
 -- Exercice 3 --
 
 --Afficher le nombre de framework qu'a un langage.
+SELECT languages.name, COUNT(*)
+-> FROM `languages`
+-> INNER JOIN `frameworks`
+-> ON languages.id = frameworks.languagesId
+-> GROUP BY languages.name;
+
+-- Exercice 4 --
+
+--Afficher les langages ayant plus de 3 frameworks.
+SELECT languages.name, COUNT(*) AS `framework`
+-> FROM `languages`
+-> INNER JOIN `frameworks`
+-> ON languages.id = frameworks.languagesId
+-> GROUP BY languages.name
+-> HAVING `framework` > 3;
